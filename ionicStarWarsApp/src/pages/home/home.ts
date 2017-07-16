22526a424f9c7f149ebd5a1f2e7a4c6215821380
David Siers <davidsiers@gmail.com>
2017-07-15 19:07:36 -0700

    Working

--- ionicStarWarsApp/src/pages/home/home.ts
+++ ionicStarWarsApp/src/pages/home/home.ts
@@ -7,8 +7,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  myVariable: string = 'The force is with me!';
 
-  }
+  constructor(public navCtrl: NavController) {}
+
+  updateMyValue() {
+      this.myVariable = 'Now the force is even stronger!';
+    }
 
 }
